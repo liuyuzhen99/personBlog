fdd46530e02a6df38b358b309f9d17584084d2a0
liuyuzhen99 <liuyuzhenbmjt@163.com>
2024-04-25 21:57:07 +0800

    css markdown viewer with github-markdown-css

--- src/pages/cardPage/cardPage.jsx
+++ src/pages/cardPage/cardPage.jsx
@@ -1,36 +1,65 @@
 import React from "react";
 import { useState } from "react";
-import BytemdViewer from "../../components/bytemdViewer";
-import BytemdEditor from "../../components/bytemdEditor";
-import { CiEdit } from "react-icons/ci";
-import { MdCancelPresentation } from "react-icons/md";
-import { GiConfirmed } from "react-icons/gi";
+// import BytemdViewer from "../../components/bytemdViewer";
+// import BytemdEditor from "../../components/bytemdEditor";
+// import { CiEdit } from "react-icons/ci";
+// import { MdCancelPresentation } from "react-icons/md";
+// import { GiConfirmed } from "react-icons/gi";
+import { Viewer, Editor } from "@bytemd/react";
+import "bytemd/dist/index.css";
+import "github-markdown-css";
 import "./cardPage.css";
 
 function cardPage() {
-  const [showEditPage, setShowEditPage] = useState(true);
-  const [buttonField, setButtonField] = useState(true);
+  const [content, setContent] = useState("# Hello, ByteMD!");
+  const [previousContent, setPreviousContent] = useState("");
+  const [editMode, setEditMode] = useState(true);
 
-  const handleBtnClick = () => {
-    setShowEditPage(!showEditPage);
-    setButtonField(!buttonField);
+  const handleEdit = () => {
+    setEditMode(true);
   };
+
+  const handlePreview = () => {
+    setEditMode(false);
+  };
+
+  const handleCancel = () => {
+    setContent(previousContent);
+    setEditMode(false);
+  };
+
+  const handleSave = () => {
+    setPreviousContent(content);
+    setEditMode(false);
+  };
+
   return (
-    <div>
-      <div className="top-bar">
-        <text>tag</text>
-        <button className="edit-btn" onClick={handleBtnClick}>
-          {buttonField ? (
-            <CiEdit />
-          ) : (
-            <div>
-              <MdCancelPresentation />
-              <GiConfirmed />
+    <div className="bytemd-right">
+      <div className="bytemd-navbar">
+        {editMode ? (
+          <div className="editMode-navbar">
+            <text>tag</text>
+            <div className="btn-box">
+              <button onClick={handleSave}>确定</button>
+              <button onClick={handleCancel}>取消</button>
             </div>
-          )}
-        </button>
+          </div>
+        ) : (
+          <div className="uneditMode-navbar">
+            <span>tag</span>
+            <button onClick={handleEdit}>编辑</button>
+          </div>
+        )}
       </div>
-      <div>{showEditPage ? <BytemdEditor /> : <BytemdViewer />}</div>
+      <div className="bytemd-content">
+        {editMode ? (
+          <Editor value={content} onChange={setContent} />
+        ) : (
+          <div className="bytemd-viewer">
+            <Viewer value={content} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
